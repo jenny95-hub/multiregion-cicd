14baaf7876f8159e8a7f792aa26e782e8c550c2d
JennyThomas <99955653+jenny95-hub@users.noreply.github.com>
2026-06-18 22:11:04 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello();
-  return "Welcome to my world"
+def home():
+
+    data = {
+        "application": "CloudOps Dashboard",
+        "environment": "Production",
+        "region": os.getenv("AWS_REGION", "ap-south-1"),
+        "version": "v1.0.0",
+        "deployment_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "status": "Healthy",
+        "pipeline": "Successful"
+    }
+
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-  appp.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
